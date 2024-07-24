4d75572b5ab3698e4b0c6d619f6ad46cfd2c30dd
fath1998-ctrl <152989236+fath1998-ctrl@users.noreply.github.com>
2024-07-24 08:35:18 +0700

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,52 +1,21 @@
-var cacheAll = false;
-var CACHE_NAME = 'webapk-cache';
-var urlsToCache = [
-	'/',
-	'/wp-content/uploads/2020/08/cropped-SC_Logo-192x192.png',
-	'/wp-content/uploads/2020/08/Supercoders-2x1_Transparent1.png',
-];
-var urlsNotToCache = [
-];
-
-// Install Event
 self.addEventListener('install', function(event) {
-	console.log("[SW] install event: ",event);
-	// Perform install steps
-	event.waitUntil(
-		caches.open(CACHE_NAME).then(
-			function(cache) {
-				console.log('[SW] Opened cache: ',cache);
-				return cache.addAll(urlsToCache);
-			}
-		)
-	);
+  event.waitUntil(
+    caches.open('offline-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html', // Replace with your main HTML file
+        '/path/to/your/styles.css', // Replace with your CSS file
+        '/path/to/your/script.js', // Replace with your JavaScript file
+        // Add paths to other assets you want to cache for offline use
+      ]);
+    })
+  );
 });
 
-
-// Fetch Event
 self.addEventListener('fetch', function(event) {
-	console.log("[SW] fetch event: ",event);
-	event.respondWith(
-		caches.match(event.request).then(
-			function(response) {
-				if (response) return response;
-				else if (!cacheAll || urlsNotToCache.indexOf(event.request) !== -1) return fetch(event.request);
-				else {
-					fetch(event.request).then(
-						function(response) {
-							if(!response || response.status !== 200 || response.type !== 'basic') return response;
-							var responseToCache = response.clone();
-							caches.open(CACHE_NAME).then(
-								function(cache) {
-									cache.put(event.request, responseToCache);
-								}
-							);
-							return response;
-						}
-					);
-				}
-			}
-		)
-	);
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });
-
